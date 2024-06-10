13c6e015f0c54dc83fc3abc908accd17e523c9ef
Aswinraj <aswinrajr07@gmail.com>
2024-06-11 01:06:14 +0530

    Mongodb Atlas added

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const dbConnect = (req, res) => {
   mongoose
     .connect(
-      "mongodb+srv://aswinrajr07:yPXdXgF4ThtnerDz@cluster0.xf8fs26.mongodb.net/FindMyHome",
+      `${process.env.DATABASE_CONNECTION}`,
       { useNewUrlParser: true, useUnifiedTopology: true }
     )
     .then(() => {
