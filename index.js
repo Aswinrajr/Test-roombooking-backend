756cc4a23902efdf474dfdc230164c1ea7782373
Aswinraj <aswinrajr07@gmail.com>
2024-06-12 12:48:27 +0530

    vercel check

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://find-my-home-frontend.vercel.app/",
     credentials: true
   }));
 app.disable('x-powered-by');
