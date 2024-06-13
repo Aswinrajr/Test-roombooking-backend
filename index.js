1113bbc7e9005a62be150479fc9690c54674db96
Aswinraj <aswinrajr07@gmail.com>
2024-06-13 19:25:04 +0530

    link changed

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://3.27.136.143:5173",
     credentials: true
   }));
 app.disable('x-powered-by');
