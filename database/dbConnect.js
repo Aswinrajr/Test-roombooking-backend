fbd2a6a2169f926a355b1813b08671ced3523197
Aswinraj <aswinrajr07@gmail.com>
2024-06-14 02:11:12 +0530

    Database reconnected

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -1,34 +1,18 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-
-const dbConnect = (req, res) => {
+const dbConnect = () => {
   mongoose
-    .connect(
-     `${process.env.DATABASE_CONNECTION}`,
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    )
+    .connect(process.env.DATABASE_CONNECTION, {
+      serverSelectionTimeoutMS: 30000, // 30 seconds
+      socketTimeoutMS: 45000, // 45 seconds
+    })
     .then(() => {
-      console.log("connected to the database atlas");
-      // console.log("connected to the database");
+      console.log("Connected to the database Atlas");
     })
     .catch((err) => {
       console.error("Error in connecting the database", err);
     });
 };
-
-
-
-
-// const dbConnect = (req, res) => {
-//   mongoose
-//     .connect(`${process.env.DATABASE_CONNECTION}`)
-//     .then(() => {
-//       console.log("Connected to database");
-//     })
-//     .catch((err) => {
-//       console.log("Error in conncting the datbase", err);
-//     });
-// };
 
 module.exports = dbConnect;
