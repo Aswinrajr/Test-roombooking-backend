6cd5d8f99a5b02d6d04ce3d7f97306da4ba2607f
Aswinraj <aswinrajr07@gmail.com>
2024-06-14 12:35:18 +0530

    Database Atlas  reconnected

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -6,7 +6,8 @@ const dbConnect = () => {
 
   mongoose
     .connect(
-      "mongodb+srv://findmyhomeadmin:admin123@cluster0.xf8fs26.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/FindMyHome",
+      // "mongodb+srv://findmyhomeadmin:admin123@cluster0.xf8fs26.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/FindMyHome",
+      "mongodb+srv://findmyhomeadmin:admin123@cluster0.xf8fs26.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     )
     .then(() => {
       console.log("Connected to the database Atlas");
