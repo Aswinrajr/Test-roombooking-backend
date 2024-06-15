bd2c72a002e0339f276f0518c6fccfa924055662
Aswinraj <aswinrajr07@gmail.com>
2024-06-15 10:51:44 +0530

    data base connection check

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -4,7 +4,13 @@ require("dotenv").config();
 const dbConnect = () => {
   console.log("Welcome to database")
   mongoose
-    .connect("mongodb+srv://aswinrajr07:yPXdXgF4ThtnerDz@cluster0.xf8fs26.mongodb.net/FindMyHome", {
+    .connect(
+      // "mongodb+srv://aswinrajr07:yPXdXgF4ThtnerDz@cluster0.xf8fs26.mongodb.net/FindMyHome"
+          "mongodb+srv://aswinrajr07:BHSHXzU0W4x263Qx@cluster0.xf8fs26.mongodb.net/FindMyHome?retryWrites=true&w=majority&appName=Cluster0"
+
+      
+      
+      , {
       serverSelectionTimeoutMS: 30000, 
       socketTimeoutMS: 45000, 
     })
