5de2c7c4458fef265e8e507127ce78c39080e519
Aswinraj <aswinrajr07@gmail.com>
2024-06-15 17:57:15 +0530

    database connection changed

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -2,15 +2,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const dbConnect = () => {
-  console.log("Welcome to database");
-  console.log("Attempting to connect to MongoDB with URI:", process.env.DATABASE_CONNECTION);
-  
+  console.log("Welcome to database")
   mongoose
-    .connect(process.env.DATABASE_CONNECTION, {
-      serverSelectionTimeoutMS: 30000,
-      socketTimeoutMS: 45000,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    .connect(
+      // "mongodb+srv://aswinrajr07:yPXdXgF4ThtnerDz@cluster0.xf8fs26.mongodb.net/FindMyHome"
+          "mongodb+srv://aswinrajr07:554nvlr8Bwc08DrQ@cluster0.xf8fs26.mongodb.net/FindMyHome?retryWrites=true&w=majority&appName=Cluster0"
+      , {
+      serverSelectionTimeoutMS: 30000, 
+      socketTimeoutMS: 45000, 
     })
     .then(() => {
       console.log("Connected to the database Atlas");
@@ -18,10 +17,6 @@ const dbConnect = () => {
     .catch((err) => {
       console.error("Error in connecting the database", err);
     });
-
-  mongoose.connection.on('error', (err) => {
-    console.error('Mongoose connection error:', err);
-  });
 };
 
 module.exports = dbConnect;
