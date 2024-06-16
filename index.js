3eb91c21a9cbb1fe2a2bccfbe553b463aa8dd900
Aswinraj <aswinrajr07@gmail.com>
2024-06-16 12:20:32 +0530

    database connection string  cors changed

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    // origin: "https://www.findmyhomestay.online",
-     origin: "http://localhost:5173",
+    origin: "https://www.findmyhomestay.online",
+    //  origin: "http://localhost:5173",
     credentials: true
   }));
 app.disable('x-powered-by');
