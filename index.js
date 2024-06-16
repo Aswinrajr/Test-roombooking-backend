73192c0ff7762353d8cff45ad17cf867f124200c
Aswinraj <aswinrajr07@gmail.com>
2024-06-16 21:12:10 +0530

    searching for ip in mutiple origins

--- index.js
+++ index.js
@@ -1,40 +1,54 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser =require("cookie-parser");
-const { json } = require("body-parser");
-require('dotenv').config() ;
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
+const adminRoute = require("./route/adminRoute/adminRoute");
+const providerRoute = require("./route/providerRoute/providerRoute");
+const userRoute = require("./route/userRoute/userRoute");
+const chatRoute = require("./route/chatRoute/chatRoute");
+const dataBase = require("./database/dbConnect");
+const logIp = require("./middleware/logIp");
 
-
-const adminRoute = require("./route/adminRoute/adminRoute")
-const providerRoute = require('./route/providerRoute/providerRoute')
-const userRoute = require('./route/userRoute/userRoute')
-const chatRoute= require("./route/chatRoute/chatRoute")
-const dataBase = require('./database/dbConnect')
-const logIp = require("./middleware/logIp")
 const app = express();
-app.use(logIp)
+const PORT = process.env.PORT || 1997;
 
-dataBase()
+app.set("trust proxy", true);
+
+app.use(logIp);
+
+dataBase();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
-    origin: "https://www.findmyhomestay.online",
-    //  origin: "http://localhost:5173",
-    credentials: true
-  }));
-app.disable('x-powered-by');
+
+const allowedOrigins = [
+  "https://www.findmyhomestay.online",
+  "http://localhost:5173",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.disable("x-powered-by");
 
 app.use("/uploads", express.static("uploads"));
-const PORT = process.env.PORT||1997;
 
-app.use("/admin",adminRoute)
-app.use('/provider',providerRoute)
-app.use("/",userRoute)
-// app.use("/chat",chatRoute)
+app.use("/admin", adminRoute);
+app.use("/provider", providerRoute);
+app.use("/", userRoute);
+// app.use("/chat", chatRoute);
 
-
-app.listen(PORT,()=>{
-    console.log(`Application is running on port address:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Application is running on port address: ${PORT}`);
 });
